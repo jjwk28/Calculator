fea024e1943655fe1bc1061b71e412639a1e8379
JangJiWook <swjw28@naver.com>
2021-11-01 11:22:00 +0900

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridLayout;
@@ -14,11 +15,13 @@ import javax.swing.SwingConstants;
 
 public class Main extends JFrame {
 	
+	// 기본 사칙연산만 가능, 괄호연산 불가
+	
 	static String[] button = {"7", "8", "9", "÷", "4", "5", "6", "×",
 							  "1", "2", "3", "-", "C", "0", "=", "+"};
-	static JLabel lb;
-	static String result = "";
-	static boolean TF;
+	static JLabel lb1, lb2; // lb1 : 계산 식 출력, lb2 : 계산 결과 출력
+	static String result = ""; // 계산 결과
+	static boolean TF; // "="을 누르는 경우 true로 전환
 	
 	public Main() {
 		setTitle("계산기");
@@ -27,23 +30,42 @@ public class Main extends JFrame {
 		Container c = getContentPane();
 		c.setLayout(new GridLayout(5, 1));
 		
-		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
-		JPanel p3 = new JPanel();
-		JPanel p4 = new JPanel();
-		JPanel p5 = new JPanel();
+		JPanel p1 = new JPanel(); // 계산 식 및 결과 표시
+		JPanel p2 = new JPanel(); // 7, 8, 9, ÷
+		JPanel p3 = new JPanel(); // 4, 5, 6, ×
+		JPanel p4 = new JPanel(); // 1, 2, 3, -
+		JPanel p5 = new JPanel(); // C, 0, =, +
 		
-		p1.setLayout(new GridLayout(1, 1));
-		lb = new JLabel("0");
-		lb.setFont(new Font("맑은고딕", Font.BOLD, 30));
-		lb.setVerticalAlignment(SwingConstants.CENTER);
-		lb.setHorizontalAlignment(SwingConstants.RIGHT);
-		p1.add(lb);
+		p1.setLayout(new GridLayout(2, 1));
+		lb1 = new JLabel("0");
+		lb1.setFont(new Font("맑은고딕", Font.BOLD, 20));
+		lb1.setOpaque(true);
+		lb1.setForeground(Color.gray);
+		lb1.setBackground(Color.black);
+		lb1.setVerticalAlignment(SwingConstants.CENTER);
+		lb1.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		lb2 = new JLabel("0");
+		lb2.setFont(new Font("맑은고딕", Font.BOLD, 40));
+		lb2.setOpaque(true);
+		lb2.setForeground(Color.white);
+		lb2.setBackground(Color.black);
+		lb2.setVerticalAlignment(SwingConstants.CENTER);
+		lb2.setHorizontalAlignment(SwingConstants.RIGHT);
+		
+		p1.add(lb1);
+		p1.add(lb2);
 		
 		p2.setLayout(new GridLayout(1, 4));
 		for(int i = 0; i < 4; i++) {
 			JButton btn = new JButton(button[i]);
 			btn.setFont(new Font("맑은고딕", Font.BOLD, 30));
+			if(i % 4 == 3) {
+				btn.setBackground(new Color(234, 237, 238));
+			}
+			else {
+				btn.setBackground(Color.white);
+			}
 			btn.addActionListener(new MyActionListener());
 			p2.add(btn);
 		}
@@ -51,6 +73,12 @@ public class Main extends JFrame {
 		for(int i = 4; i < 8; i++) {
 			JButton btn = new JButton(button[i]);
 			btn.setFont(new Font("맑은고딕", Font.BOLD, 30));
+			if(i % 4 == 3) {
+				btn.setBackground(new Color(234, 237, 238));
+			}
+			else {
+				btn.setBackground(Color.white);
+			}
 			btn.addActionListener(new MyActionListener());
 			p3.add(btn);
 		}
@@ -58,6 +86,12 @@ public class Main extends JFrame {
 		for(int i = 8; i < 12; i++) {
 			JButton btn = new JButton(button[i]);
 			btn.setFont(new Font("맑은고딕", Font.BOLD, 30));
+			if(i % 4 == 3) {
+				btn.setBackground(new Color(234, 237, 238));
+			}
+			else {
+				btn.setBackground(Color.white);
+			}
 			btn.addActionListener(new MyActionListener());
 			p4.add(btn);
 		}
@@ -65,6 +99,15 @@ public class Main extends JFrame {
 		for(int i = 12; i < 16; i++) {
 			JButton btn = new JButton(button[i]);
 			btn.setFont(new Font("맑은고딕", Font.BOLD, 30));
+			if(i % 4 == 3) {
+				btn.setBackground(new Color(234, 237, 238));
+			}
+			else if(i % 4 != 1) {
+				btn.setBackground(new Color(132, 190, 247));
+			}
+			else {
+				btn.setBackground(Color.white);
+			}
 			btn.addActionListener(new MyActionListener());
 			p5.add(btn);
 		}
@@ -90,19 +133,21 @@ public class Main extends JFrame {
 							TF = false;
 						}
 						result += b.getText();
-						lb.setText(result);
+						lb2.setText(result);
 						break;
 					}
 					else if(i == 12) { // 'C'를 눌렀을 경우
 						result = "0";
-						lb.setText(result);
+						lb1.setText(result);
+						lb2.setText(result);
 						result = "";
 						TF = false;
 						break;
 					}
 					else if(i == 14) { // '='를 눌렀을 경우
+						lb1.setText(result);
 						result = Integer.toString(calculator());
-						lb.setText(result);
+						lb2.setText(result);
 						TF = true;
 						break;
 					}
@@ -112,10 +157,13 @@ public class Main extends JFrame {
 							TF = false;
 						}
 						result += b.getText();
-						lb.setText(result);
+						lb2.setText(result);
 						break;
 					}
 				}
+			}
+			if(result.length() >= 20) { // 계산 식이 20자리가 넘어가면 글자크기 축소
+				lb2.setFont(new Font("맑은고딕", Font.BOLD, 30));
 			}
 		}
 	}
@@ -155,7 +203,7 @@ public class Main extends JFrame {
 		}
 		return 0;
 	}
-	public static ArrayList<String> postfix(String result) { // 후위연산
+	public static ArrayList<String> postfix(String result) { // 중위연산 -> 후위연산
 		String temp = ""; // 숫자 임시 저장
 		ArrayList<String> list = new ArrayList<>(); // 후위 표기법 저장
 		Stack<Character> op = new Stack<>(); // 연산자 저장
